87f2b57712e102b7a3ab1abfac4417d03f0fb48f
Darwin-II <30838518+Darwin-II@users.noreply.github.com>
2020-10-10 13:34:40 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+cd dwm-6.2
+make
+sudo make install
+cd ../
+cd slstatus
+make
+sudo make install
+cd ../
+cd st-virgin
+make
+sudo make install
+cd ../
+cp -r xinitrc ~/.xinitrc
+cp -r xbindkeysrc ~/.xbindkeysrc
+Echo Quick tutorial
+nano binds.txt
